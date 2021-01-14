9024cf83933b9027962f8456162bbca7283ca41b
kuma <inataku0121@gmail,com>
2021-01-14 20:23:26 +0900

    #2トップページ編集

--- main.js
+++ main.js
@@ -3,17 +3,28 @@ $(function() {
   topBtn.hide();
   //スクロールが100に達したらボタン表示
   $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
+      if ($(this).scrollTop() > 0) {
           topBtn.fadeIn();
       } else {
           topBtn.fadeOut();
       }
   });
+
+  $(window).scroll(function (){
+    $('.fadein').each(function(){
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 200){
+            $(this).addClass('scrollin');
+        }
+    });
+});
   //スクロールしてトップ
   topBtn.click(function () {
       $('body,html').animate({
           scrollTop: 0
-      }, 500);
+      }, 1000);
       return false;
   });
 });
